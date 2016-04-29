70749478603b8c6929043ac787b021e2d63a4c05
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-29 14:27:21 -0700

    always make the same route mistake for post

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -6,4 +6,17 @@ class CreaturesController < ApplicationController
   def new
     @creature = Creature.new
   end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creatures_path
+    end
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 end
