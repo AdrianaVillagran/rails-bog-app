6785601587f8276473b318761623e84cad44ee23
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-01 23:27:24 -0700

    added index and new routes and action views

---
+++ bog_app3/db/seeds.rb
@@ -0,0 +1,5 @@
+Creature.destroy_all
+
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
+Creature.create({name: "Yoda", description: "Little green man"})
